e7ddef5591206784163628ed1ca192dc3515a138
Afevis <ShizCalev@users.noreply.github.com>
2026-09-02 17:39:55 -0600

    version bump

--- src/resources/version.h
+++ src/resources/version.h
@@ -10,7 +10,7 @@
 
 #define VERSION_MAJOR 0
 #define VERSION_MINOR 2
-#define VERSION_PATCH 0
+#define VERSION_PATCH 1
 #define VERSION_CI_BUILD 0
 
 /// Current release version of MGSPatriotFix at time of compile.
